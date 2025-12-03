bc66b532a9faae895b1d3699577e8c17ced4c47f
lucariggi92 <riggiluca92@gmail.com>
2025-12-03 17:06:51 +0100

    feat: milestone 1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [dati, setDati] = useState([])
+
+
+  //FETCHING DEI DATI
+  function fetchDati() {
+    axios.get("https://lanciweb.github.io/demo/api/actors/").then(
+      (resp) => {
+        setDati(resp.data); //data è l'oggetto 
+      }).catch(err => console.log("ERRORE:", err));
+  }
+
+  useEffect(fetchDati, []); //fetchdati solo al primo rendering
+
+
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header className='bg-warning py-2'><div className='container'> <h1>Lista Attori</h1></div></header>
+
+
+      <div className="container text-center">
+        <div className="row">
+
+
+            {dati.map((oggetto, index) => (
+                        <div className="col" key={oggetto.id}>
+              <div className="card" style={{ width: "18rem" }} >
+                <img src={oggetto.image} className="card-img-top" alt="{oggetto.name}" />
+                <div className="card-body">
+                  <h5 className="card-title">{oggetto.name}</h5>
+                  <p className="card-text">{oggetto.birth_year}</p>
+                  <p className="card-text">{oggetto.nationality}</p>
+                  <p className="card-text">{oggetto.biography}</p>
+                  <p className="card-text">{oggetto.awards}</p>
+  
+
+
+                </div>
+              </div>
+          </div>     
+
+      ))}
+ </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
